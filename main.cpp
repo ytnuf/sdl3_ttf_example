a87a167350a7719bb4832ef9bad6c4c749e536db
ytnuf <161308826+ytnuf@users.noreply.github.com>
2025-03-29 13:20:07 +0000

    Use SDL3 ttf

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 
 #include <SDL3/SDL_init.h>
+#include <SDL3_ttf/SDL_ttf.h>
 
 #include <iostream>
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
+    TTF_Quit();
     SDL_Quit();
-    std::cout<<"hello SDL"<<std::endl;
+    std::cout<<"hello SDL ttf"<<std::endl;
     return 0;
 }
