e31325939e10739d045a972278c1239feff22033
ytnuf <161308826+ytnuf@users.noreply.github.com>
2025-03-29 13:03:57 +0000

    Use SDL

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 
+#include <SDL3/SDL_init.h>
+
 #include <iostream>
 
 int main() {
-    std::cout<<"hello"<<std::endl;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Quit();
+    std::cout<<"hello SDL"<<std::endl;
     return 0;
 }
